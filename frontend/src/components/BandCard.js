55facfb832a958f549263ea0d5700f947ea56959
therealilyaskhan <therealilyaskhan@hotmail.com>
2021-07-16 19:18:55 +0500

    band  card completey responsive

--- frontend/src/components/BandCard.js
+++ frontend/src/components/BandCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { useMediaQuery } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Collapse from '@material-ui/core/Collapse';
@@ -7,18 +7,11 @@ import Box from '@material-ui/core/Box';
 import CardContainer from './Card/CardContainer';
 import CardAvatarResponsive from './Card/CardAvatarResponsive';
 import CardContentResponsive from './Card/CardContentResponsive';
-import CardControlsResponsive from './Card/CardControlsResponsive';
 import IWT from './IWT';
 import StyledButton from './StyledButton';
 import PersonIcon from '@material-ui/icons/Person';
-
 import IconButton from '@material-ui/core/IconButton';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import { red } from '@material-ui/core/colors';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -92,7 +85,6 @@ const useStyles = makeStyles((theme) => ({
 export default function BandCard({ band }) {
   const { ID, owner, photo, progress, description, techs, members } = band;
   const classes = useStyles();
-  const theme = useTheme();
   const tabletCard = useMediaQuery('(max-width:800px)');
   const ipadCard = useMediaQuery('(max-width:740px)');
   const mobileCard = useMediaQuery('(max-width:640px)');
@@ -368,80 +360,3 @@ export default function BandCard({ band }) {
     </CardContainer >
   );
 }
-
-
-
-
-
-// import React from 'react';
-// import { makeStyles, useTheme } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import IconButton from '@material-ui/core/IconButton';
-// import Typography from '@material-ui/core/Typography';
-// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-// import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-// import SkipNextIcon from '@material-ui/icons/SkipNext';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//   },
-//   details: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//   },
-//   content: {
-//     flex: '1 0 auto',
-//   },
-//   cover: {
-//     width: 151,
-//   },
-//   controls: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     paddingLeft: theme.spacing(1),
-//     paddingBottom: theme.spacing(1),
-//   },
-//   playIcon: {
-//     height: 38,
-//     width: 38,
-//   },
-// }));
-
-// export default function MediaControlCard() {
-//   const classes = useStyles();
-//   const theme = useTheme();
-
-//   return (
-//     <Card className={classes.root}>
-//       <div className={classes.details}>
-//         <CardContent className={classes.content}>
-//           <Typography component="h5" variant="h5">
-//             Live From Space
-//           </Typography>
-//           <Typography variant="subtitle1" color="textSecondary">
-//             Mac Miller
-//           </Typography>
-//         </CardContent>
-//         <div className={classes.controls}>
-//           <IconButton aria-label="previous">
-//             {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-//           </IconButton>
-//           <IconButton aria-label="play/pause">
-//             <PlayArrowIcon className={classes.playIcon} />
-//           </IconButton>
-//           <IconButton aria-label="next">
-//             {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-//           </IconButton>
-//         </div>
-//       </div>
-//       <CardMedia
-//         className={classes.cover}
-//         image="/static/images/cards/live-from-space.jpg"
-//         title="Live from space album cover"
-//       />
-//     </Card>
-//   );
-// }
