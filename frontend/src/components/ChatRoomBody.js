2030546b290665c36aa2e4ea2bf45452d2f222c9
therealilyaskhan <therealilyaskhan@hotmail.com>
2021-08-05 00:19:53 +0500

    chat room module finished

--- frontend/src/components/ChatRoomBody.js
+++ frontend/src/components/ChatRoomBody.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 
 import Message from './Message';
 import Image from '../assets/images/user_img.jpg';
 
+
+const useStyles = makeStyles({
+  conversation: {
+    flex: '0 1 auto',
+    overflowY: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+  }
+});
+
 export default function ChatRoomBody({ chatRoomID, className }) {
+
+  const classes = useStyles();
 
   // fetch the latest certain amount of messages using the chatRoomID
 
@@ -16,7 +30,8 @@ export default function ChatRoomBody({ chatRoomID, className }) {
       userName: 'Kannaj',
       userImage: Image,
       time: 'sent at',
-      message: 'Relevant stuff about dinosaurs'
+      message: 'Relevant stuff about dinosaurs',
+      isMyMessage: false
     },
     {
       msgID: 2,
@@ -24,22 +39,106 @@ export default function ChatRoomBody({ chatRoomID, className }) {
       userName: 'Kannaj',
       userImage: Image,
       time: 'sent at',
-      message: 'The popup first show up on the center then is dismissed and replaced by another popup on the bottom of the screen. I initially thought it was a CSS issue'
-    }
+      message: 'The popup first show up on the center then is dismissed and replaced by another popup on the bottom of the screen. I initially thought it was a CSS issue',
+      isMyMessage: false
+    },
+    {
+      msgID: 3,
+      userID: 2,
+      userName: 'Ilyas',
+      userImage: Image,
+      time: 'sent at',
+      message: 'What kinda work you were doing in the cuba Tony ? things like this that some construction business.',
+      isMyMessage: true
+    },
+    {
+      msgID: 4,
+      userID: 3,
+      userName: 'Robert',
+      userImage: Image,
+      time: 'sent at',
+      message: 'What kinda work you were doing in the cuba Tony ? things like this that some construction business.',
+      isMyMessage: false
+    },
+    {
+      msgID: 5,
+      userID: 2,
+      userName: 'Ilyas',
+      userImage: Image,
+      time: 'sent at',
+      message: 'The popup first show up on the center then is dismissed and replaced by another popup on the bottom of the screen. I initially thought it was a CSS issue',
+      isMyMessage: true
+    },
+    {
+      msgID: 6,
+      userID: 2,
+      userName: 'Ilyas',
+      userImage: Image,
+      time: 'sent at',
+      message: 'What kinda work you were doing ?',
+      isMyMessage: true
+    },
+    {
+      msgID: 7,
+      userID: 2,
+      userName: 'Kannaj',
+      userImage: Image,
+      time: 'sent at',
+      message: 'What kinda work you were doing in the cuba Tony ? things like this that some construction business.The popup first show up on the center then is dismissed.',
+      isMyMessage: false
+    },
+    {
+      msgID: 8,
+      userID: 3,
+      userName: 'Robert',
+      userImage: Image,
+      time: 'sent at',
+      message: 'What kinda work you were doing in the cuba Tony ? things like this that some construction business.',
+      isMyMessage: false
+    },
+    {
+      msgID: 9,
+      userID: 2,
+      userName: 'Ilyas',
+      userImage: Image,
+      time: 'sent at',
+      message: 'What kinda work you were doing ?',
+      isMyMessage: true
+    },
   ];
 
   return (
-    <Box p={4} className={className || ''}>
-      {
-        messages.map((message) => {
-          return (
-            <Message
-              key={message.msgID}
-              msg={message}
-            />
-          );
-        })
-      }
-    </Box>
+    <Box
+      m={0}
+      p={0}
+      className={className || ''}
+      display='flex'
+      flexDirection='column'
+      justifyContent='flex-end'
+      alignItems='stretch'
+      alignContent='stretch'
+      height='100%'
+      width='100%'
+      minHeight='0px'
+      minWidth='0px'
+    >
+      <Box
+        p={4}
+        pb={0}
+        m={0}
+        className={classes.conversation}
+      >
+        {
+          messages.map((message) => {
+            return (
+              <Message
+                key={message.msgID}
+                msg={message}
+              />
+            );
+          })
+        }
+      </Box>
+    </Box >
   );
 }
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -3,6 +3,7 @@ import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Typography from '@material-ui/core/Typography';
+import { lightBlue } from '@material-ui/core/colors';
 
 import CardContent from './Card/CardContent';
 import CardContainer from './Card/CardContainer';
@@ -14,11 +15,20 @@ const useStyles = makeStyles({
     border: '1px solid #707070'
   },
   msgBox: {
-    width: 'fit-content'
+    width: 'fit-content',
   },
-  msg: {
-    margin: '-2px 0px -2px',
-    lineHeight: '18px'
+  myMsg: {
+    margin: '-1px 0px -1px',
+    lineHeight: '18px',
+    color: '#fff'
+  },
+  otherMsg: {
+    margin: '-1px 0px -1px',
+    lineHeight: '18px',
+    color: '#011627'
+  },
+  bgMyMsg: {
+    backgroundColor: lightBlue[600]
   }
 });
 
@@ -28,56 +38,66 @@ export default function Message({ msg }) {
   const md = useMediaQuery('(max-width:650px)'); //90%
   const sm = useMediaQuery('(max-width:620px)'); //95%
   const xs = useMediaQuery('(max-width:599.95px)'); //85%
-  const xss = useMediaQuery('(max-width:500px)'); //95%
 
-  const msgWidth = xss ? '95%' : xs ? '85%' : sm ? '95%' : md ? '90%' : lg ? '85%' : '80%';
+  const msgWidth = xs ? '75%' : sm ? '70%' : md ? '80%' : lg ? '75%' : '70%';
 
   const classes = useStyles();
 
-  const { userImage, message } = msg;
+  const { userImage, message, isMyMessage } = msg;
   return (
     <Box
-      display='flex'
-      alignItems='center'
       mb={2.5}
+      flexGrow={0}
+      flexShrink={0}
+      flexBasis='auto'
+      display='flex'
+      justifyContent={isMyMessage ? 'flex-end' : 'flex-start'}
+      alignItems='center'
       flexWrap='nowrap'
-      width={msgWidth}
     >
-      <Box
-        flexShrink={0}
-        flexGrow={0}
-      >
-        <CardAvatarResponsive
-          customStyles={classes.userAvatar}
-          src={userImage}
-          alt="User image"
-          aria-label="user-image"
-          size={62}
-        />
-      </Box>
+      {
+        !isMyMessage ?
+          <Box
+            flexShrink={0}
+            flexGrow={0}
+          >
+            <CardAvatarResponsive
+              customStyles={classes.userAvatar}
+              src={userImage}
+              alt="User image"
+              aria-label="user-image"
+              size={62}
+            />
+          </Box> :
+          null
+      }
 
       <Box
         flexGrow={1}
-        flexBasis='100%'
+        flexShrink={1}
+        flexBasis={msgWidth}
+        maxWidth={msgWidth}
+        display='flex'
+        alignItems='center'
+        justifyContent={isMyMessage ? 'flex-end' : 'flex-start'}
       >
         <CardContainer
-          customStyles={classes.msgBox}
+          customStyles={`${classes.msgBox} ${isMyMessage ? classes.bgMyMsg : ''}`}
           elevation={1}
           br={40}
           pt={8}
           pb={8}
           mt={0}
           mb={0}
-          ml={20}
+          ml={isMyMessage ? 0 : 20}
         >
           <CardContent
             pb={16}
           >
             <Typography
-              className={classes.msg}
+              className={isMyMessage ? classes.myMsg : classes.otherMsg}
               variant='subtitle2'
               component='div'
-              color='textPrimary'
             >
               {message}
             </Typography>
