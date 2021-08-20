011412f206881d27f8ae8f0783fe9fa6b64e26b3
therealilyaskhan <therealilyaskhan@hotmail.com>
2021-08-21 04:26:37 +0500

    genres content area initiated

--- frontend/src/screens/SubscribedGenresScreen.js
+++ frontend/src/screens/SubscribedGenresScreen.js
@@ -1,7 +1,20 @@
+import { makeStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'red',
+    flex: '1 1 auto'
+  }
+});
+
 export default function SubscribedGenresScreen({ genres }) {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      Subscribed Genres
-    </div>
+    <Box className={classes.root}>
+      Subscribed Genres Screen
+    </Box>
   );
 }
